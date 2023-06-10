42e32c688a74a8df67e3ba9b1fd6e130eb5cc86c
ibrahim <ibrahimmuradov24@gmail.com>
2023-06-10 17:16:17 +0400

    update home.py

--- home.py
+++ home.py
@@ -1,57 +1,43 @@
 # -*- coding: utf-8 -*-
 
 import speech_recognition as sr
-from datetime import datetime
 from playsound import playsound
 from gtts import gTTS
+from worlds import worlds
 import random
 import os
 
-r = sr.Recognizer()
-mic = sr.Microphone()
+rcognizer = sr.Recognizer()
+microphone_sound = sr.Microphone()
 
 # listen mic
 def mic_text_doc():
-    text = ''
-    with mic as m:
+    sound_text = ''
+    with microphone_sound as microphone:
         print('Listening..')
-        r.pause_threshold = 1
-        audio = r.listen(m, phrase_time_limit=5)
+        rcognizer.pause_threshold = 1
+        audio = rcognizer.listen(microphone, phrase_time_limit=5)
 
         try:
-            text = r.recognize_google(audio, language='en-EN')
+            sound_text = rcognizer.recognize_google(audio, language='en-EN')
         except sr.UnknownValueError:
             print(" -- I don't understand you -- ")
-    return text
+    return sound_text
 
 # change text data to speak
 def text_to_speak(data):
-    tts = gTTS(data, lang="en")
-    rand = random.randint(1, 1000000)
-    file = "audio-"+str(rand)+".mp3"
-    tts.save(file)
-    playsound(file)
-    os.remove(file)
-
+    sound_file = gTTS(data, lang="en")
+    generate_random_number = random.randint(1, 1000000)
+    file_name = "audio-"+str(generate_random_number)+".mp3"
+    sound_file.save(file_name)
+    playsound(file_name)
+    os.remove(file_name)
 
 flag = True
 # search world
 while flag:
     text = mic_text_doc().lower()
     print("You: ", text)
-
-    worlds = {
-        "exit": "OK, exiting program",
-        "hi": "Hello Sir",
-        "hello": "hi sir",
-        "can you hear me": "Yes, I can hear you",
-        "how are you": "I am fine and you",
-        "oru": "I am fine and you",
-        "i am also good": "Great",
-        "thank you": "Welcome",
-        "what are you doing": "I'm learning new things",
-        "what time is it": datetime.now().strftime("%H:%M")
-    }
 
     if text in worlds.keys():
         answer = worlds[text]
---
+++ worlds.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+worlds = {
+    "exit": "OK, exiting program",
+    "hi": "Hello Sir",
+    "hello": "Hi sir",
+    "can you hear me": "Yes, I can hear you",
+    "how are you": "I am fine and you",
+    "oru": "I am fine and you",
+    "i am also good": "Great",
+    "thank you": "Welcome",
+    "what are you doing": "I'm learning new things",
+    "what time is it": datetime.now().strftime("%H:%M")
+}
